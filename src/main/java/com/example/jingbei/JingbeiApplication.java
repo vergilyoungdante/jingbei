cbb8accdefa9ad0868ac4eaa08f4d2f04d52c1ff
yjb <yjb387759719@outlook.com>
2021-07-26 20:04:39 +0800

    Initial commit

---
+++ src/main/java/com/example/jingbei/JingbeiApplication.java
@@ -0,0 +1,13 @@
+package com.example.jingbei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingbeiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JingbeiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jingbei/JingbeiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jingbei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JingbeiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
